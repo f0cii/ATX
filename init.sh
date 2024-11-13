c5e6e2202cae45f76d3e9d0a433076109cb3bbb4
yl <2201441955@qq.com>
2024-11-13 09:13:45 +0800

    1

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(realpath . .magic/envs/default/lib)
 # export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH:$(realpath ./lib .magic/envs/default/lib)
 # export LD_PRELOAD=./libatx-classic.so
+=======
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(realpath .magic/envs/default/lib):$(realpath .)
+# export LD_PRELOAD=libatx-classic.so
+>>>>>>> be22614 (1)
 
